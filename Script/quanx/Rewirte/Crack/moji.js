8d6c499e10b15d9f5f26be61e9e1f525800d5b6f
WinkeyLin <56619122+WinkeyLin@users.noreply.github.com>
2021-09-30 22:03:46 +0800

    update moji

--- Script/quanx/Rewirte/Crack/moji.js
+++ Script/quanx/Rewirte/Crack/moji.js
@@ -1,5 +1,5 @@
 var obj = JSON.parse($response.body); 
 
-obj = {"result":{"result":[{"identity":"000-000-00001","privilege":{"payType":"4","purchaseDate":1632214771760},"privilegeStatus":"activated","canPay":true},{"identity":"000-002-00001","privilege":{"payType":"4","purchaseDate":1632214771760},"privilegeStatus":"Activated","canPay":true}],"code":200}}
+obj = {"result":{"result":[{"identity":"000-000-00001","privilege":{"payType":"4","purchaseDate":1632214771760},"privilegeStatus":"activated","canPay":true},{"identity":"000-002-00001","privilege":{"payType":"4","purchaseDate":1632214771760},"privilegeStatus":"activated","canPay":true}],"code":200}}
 
 $done({body:JSON.stringify(obj)});
