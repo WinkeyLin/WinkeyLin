0934fae82254d27433079aadb7c5f14df4a924ea
WinkeyLin <56619122+WinkeyLin@users.noreply.github.com>
2021-09-30 20:27:19 +0800

    update

---
+++ Script/quanx/Rewirte/Crack/xcz.js
@@ -0,0 +1,10 @@
+var obj = JSON.parse($response.body);
+
+function key() {
+    obj[arguments[0]] = arguments[1]
+    }
+
+key("lifetimeMembership", true);
+key("membership", "true");
+
+$done({body: JSON.stringify(obj)});
