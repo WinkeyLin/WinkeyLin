c35d0e9609fc2add494306f4256409c274c7e486
WinkeyLin <56619122+WinkeyLin@users.noreply.github.com>
2021-10-01 17:27:21 +0800

    Update pdfexpert.js

--- Script/quanx/Rewirte/Crack/pdfexpert.js
+++ Script/quanx/Rewirte/Crack/pdfexpert.js
@@ -1,5 +1,9 @@
-var obj = JSON.parse($response.body); 
+var obj = JSON.parse($response.body);
 
-obj = {"productId":"com.readdle.PDFExpert5.subscription.year50_pe6","subscriptionExpirationIntent":"userCancelled","receiptStatus":"ok","subscriptionExpirationDate":"13:1503/11/2099","isPDFExpert6User":false,"inAppStates":[{"productId":"com.readdle.PDFExpert5.subscription.year50_pe6","subscriptionExpirationIntent":"userCancelled","receiptStatus":"ok","subscriptionExpirationDate":"13:1503/11/2099","isEligibleForIntroPeriod":false,"originalTransactionId":"20000618444996","productName":"subscription","isInBillingRetryPeriod":false,"type":"subscription","subscriptionState":"active","subscriptionAutoRenewStatus":"autoRenewOff","isInGracePeriod":false,},],"isEligibleForIntroPeriod":false,"originalTransactionId":"20000618444996","isEligibleFor":["winback"],"isInBillingRetryPeriod":false,"type":"subscription","inAppPurchased":["com.readdle.PDFExpert5.subscription.year50_pe6"],"bundleId":"com.readdle.PDFExpert5","receiptId":1572178404000,"chargingPlatform":"iOSAppStore","subscriptionState":"active","subscriptionAutoRenewStatus":"autoRenewOff","isInGracePeriod":false}
+function key() {
+    obj[arguments[0]] = arguments[1]
+    }
 
-$done({body:JSON.stringify(obj)});
+key("inAppStates", '[{"productId":"com.readdle.PDFExpert5.subscription.year50_pe6","subscriptionExpirationIntent":"userCancelled","receiptStatus":"ok","subscriptionExpirationDate":"13:15 03/11/2099","isEligibleForIntroPeriod":false,"originalTransactionId":"20000618444996","productName":"subscription","isInBillingRetryPeriod":false,"type":"subscription","subscriptionState":"active","subscriptionAutoRenewStatus":"autoRenewOff","isInGracePeriod":false}]');
+
+$done({body: JSON.stringify(obj)});
